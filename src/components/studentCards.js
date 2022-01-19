a513fd36b7767fb09e3a1fbf380cab128037a697
vidallizet <lvidalsaldivar7855@richmondhillhs.org>
2022-01-19 03:27:28 +0000

    yearbookchanges

---
+++ src/components/studentCards.js
@@ -0,0 +1,60 @@
+import studentCard from ';
+<div className="container">
+        <h2 className="section-title">Seniors</h2>
+        <ul className="cards">
+          <li className="cards-item third">
+            <div className="card">
+              <div className="card-content">
+                <div className="card-image"></div>
+                <div className="card-container">
+                  <div className="card-title">Name M. Surname</div>
+                  <p className="card-text">"Senior quote"</p>
+                  <p className="card-text">Activities</p>
+                </div>
+              </div>
+            </div>
+          </li>
+
+          <div className="container">
+        <h2 className="section-title">Juniors</h2>
+        <ul className="cards">
+          <li className="cards-item quarter">
+            <div className="card">
+              <div className="card-content">
+                <div className="card-image"></div>
+                <div className="card-container">
+                  <div className="card-title">Name M. Surname</div>
+                  <p className="card-text">Activities</p>
+                </div>
+              </div>
+            </div>
+          </li>
+
+          <div className="container">
+        <h2 className="section-title">Sophomores</h2>
+        <ul className="cards">
+          <li className="cards-item fifth">
+            <div className="card">
+              <div className="card-content">
+                <div className="card-image"></div>
+                <div className="card-container">
+                  <div className="card-title">Name M. Surname</div>
+                  <p className="card-text">Activities</p>
+                </div>
+              </div>
+            </div>
+          </li>
+
+          <div className="container">
+        <h2 className="section-title">Freshmen</h2>
+        <ul className="cards">
+          <li className="cards-item fifth">
+            <div className="card">
+              <div className="card-content">
+                <div className="card-container">
+                  <div className="card-title">Name M. Surname</div>
+                  <p className="card-text">Activities</p>
+                </div>
+              </div>
+            </div>
+          </li>
